9abb01d88e2f2ea37a62c5a927dc88fc7e6844dd
EliasXVIII <elias.renault4@gmail.com>
2024-05-16 09:57:29 +0200

    Function GET & POST OK

--- FastAPI/users.py
+++ FastAPI/users.py
@@ -13,28 +13,47 @@ class User(BaseModel): ##El Base Model nos crea una entidad User
 
 users_list = [User(id=1 ,name="Elias",surname="Riquelme", age=36),
               User(id=2 ,name="Irene", surname="Gallego", age=29),
-              User(id=2 ,name="Nicolas", surname="Pino", age=33)]
+              User(id=3 ,name="Nicolas", surname="Pino", age=33)]
 
 """ @app.get("/usersjson")
 async def users():
     return [{"name": "Elias","surname":"Riquelme","age":36},{"name":"Irene","surname":"Gallego","age":29}] """
 
-""" @app.get("/users")
+@app.get("/users/")
 async def users():
-    return users_list """
+    return users_list
 
 @app.get("/user/{id}")##Con el {id} hago que filtre por ID
 async def user(id: int):
+    return search_user(id)
+
+##Este ejemplo trabaja con query
+@app.get("/user/")
+async def user(id: int):
+    return search_user(id)
+
+
+
+## Voy a hacer un POST
+@app.post("/user/")
+async def user(user: User):
+    if type(search_user(user.id))==User:
+        return {"error": "User already exists"}
+    else:
+        users_list.append(user)
+
+
+
+
+
+
+##Funcion para buscar usuarios.
+def search_user(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except: 
         return {"error": "User not found this is empty"}
-
-
-
-
-
 
 
 if __name__ == "__main__":
