921ce9e0b3a57b8f2ed0c36b086a0a6a2f2bf4ee
EliasXVIII <elias.renault4@gmail.com>
2024-05-17 11:55:51 +0200

    JWT ok

--- FastAPI/routers/jwt_auth_users.py
+++ FastAPI/routers/jwt_auth_users.py
@@ -1,13 +1,16 @@
 from fastapi import Depends, FastAPI, HTTPException, status
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import jwt
+from jose import JWTError, jwt
 from passlib.context import CryptContext
-from datetime import datetime, timedelta ## Para trabajar con calculos de fecha
+from datetime import datetime, timedelta ## Para trabajar con calculos de fecha(timeedelta)
 
 ##El algoritmo que voy a utilizar es HS256
 ALGORITHM = "HS256"
 ACCESS_TOKEN_DURATION = 1
+
+## para generar una palabra secreta tenemos el siguiente código que se ejecuta en consola $ openssl rand -hex 32
+SECRET = "1c85b246abedd2998d7031d11da7a33fe7221830d81ec783ec3e98bd81e7e5ef"
 
 app = FastAPI()
 
@@ -52,21 +55,52 @@ users_db = {
 def search_user_db(username: str):
     if username in users_db:
         return UserDB(**users_db[username])
+    
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username])
+
+## Intentar definir un nuevo criterio y encontrar el usuario autenticadoo
+async def auth_user(token: str = Depends(oauth2)):
+    
+    exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de auth invalidas", headers={"WWW-Authenticate":"Bearer"})
+    
+    try:
+        username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
+    except JWTError:
+        raise exception
+    
+    return search_user(username)
+
+
+## Aca coloco el Current User
+async def current_user(user : User = Depends(auth_user)):
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario inactivo")
+    return user
 
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()):
     user_db = users_db.get(form.username)
-    if not users_db:
+    if not user_db:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
     
     user = search_user_db(form.username)
-    
 
     if not crypt.verify(form.password, user.password):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta"
         )
-    
+    access_token = {"sub":user.username, "exp":datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)}
+
+    return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer"} ##access_token
 
 
-    return {"access_token": user.username, "token_type": "bearer"}
+
+##Tengo la operacion para traer mis datos
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
