c2f8ad50ce80da880644e937d77126e43e5fdcc4
EliasXVIII <elias.renault4@gmail.com>
2024-05-21 21:54:57 +0200

    Configuracion de base de datos en remoto MongoDB Atlas

--- FastAPI/db/client.py
+++ FastAPI/db/client.py
@@ -1,4 +1,17 @@
- ## importaciones
 from pymongo import MongoClient
 
-db_client = MongoClient() ##dentro de estos parentesis voy a colocar la URL de donde estaría la base de datos en caso de que este alojada en otro sitio pero en este caso sera local y no se coloca nada
+# URL de conexión a MongoDB
+mongodb_url = "mongodb+srv://elias:HicsPB8Il7jgEJ1q@cluster10.voo1tde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster10"
+
+# Crea el cliente MongoDB y hazlo accesible como db_client
+db_client = MongoClient(mongodb_url) 
+
+
+""" 
+## importaciones
+from pymongo import MongoClient
+
+
+db_client = MongoClient('mongodb+srv://elias:HicsPB8Il7jgEJ1q@cluster10.voo1tde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster10')
+##db_client = MongoClient().local ##dentro de estos parentesis voy a colocar la URL de donde estaría la base de datos en caso de que este alojada en otro sitio pero en este caso sera local y no se coloca nada
+ """
--- FastAPI/routers/users_db.py
+++ FastAPI/routers/users_db.py
@@ -1,5 +1,19 @@
-##Este lo conectaré con Mongo DB API
 
+
+from fastapi import APIRouter, HTTPException, status
+from db.models.user import User
+from db.schemas.user import user_schema, users_schema
+from db.client import db_client  # Importamos db_client desde el módulo client
+from bson import ObjectId
+
+router = APIRouter(prefix="/userdb", tags=["userdb"], responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
+
+# Selecciona la base de datos y la colección
+database = db_client['Cluster10']
+users_collection = database['users']
+
+##Este lo conectaré con Mongo DB API
+""" 
 from fastapi import APIRouter, HTTPException, status
 from db.models.user import User
 import uvicorn
@@ -7,17 +21,18 @@ from db.schemas.user import user_schema, users_schema
 from db.client import db_client ##En este paso vamos a importar de la carpeta "db"."client" la llamada db_client
 
 ##para formatear los json o los objetos a ObjectId de MongoDB utilizo lo siguiente
-from bson  import ObjectId
+from bson import ObjectId
 
 router = APIRouter(prefix="/userdb", tags=["userdb"], responses={status.HTTP_404_NOT_FOUND: {"message": "No encontrado"}})
 
-
+users_collection = db_client['mongodb+srv://elias:HicsPB8Il7jgEJ1q@cluster10.voo1tde.mongodb.net/?retryWrites=true&w=majority&appName=Cluster10']['users'] """
 
 
 ## con esta funcion busco en users todos los resultados.
 @router.get("/", response_model=list[User])
 async def users():
-    return users_schema(db_client.local.users.find())
+    return users_schema(users_collection.find())
+    #return users_schema(db_client.users.find())
 
 @router.get("/{id}")##Con el {id} hago que filtre por ID
 async def user(id: str):
@@ -45,8 +60,10 @@ async def user(user: User):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User already exists")
     user_dict = dict(user) 
     del user_dict["id"]
-    id = db_client.local.users.insert_one(user_dict).inserted_id   
-    new_user = user_schema(db_client.local.users.find_one({"_id": id}))
+    id = users_collection.insert_one(user_dict).inserted_id 
+    #id = db_client.users.insert_one(user_dict).inserted_id 
+    new_user = user_schema(users_collection.find_one({"_id": id}))  
+    #new_user = user_schema(db_client.users.find_one({"_id": id}))
     return User(**new_user)
     
 
@@ -61,8 +78,8 @@ async def user(user: User):## si quiero actualizar el usuario completo tengo que
     del user_dict["id"]
 
     try:
-        
-        db_client.local.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+        users_collection.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
+        #db_client.users.find_one_and_replace({"_id": ObjectId(user.id)}, user_dict)
 
     except:
         return {"error":"no se ha actualizado el usuario"}    
@@ -73,7 +90,8 @@ async def user(user: User):## si quiero actualizar el usuario completo tengo que
 ## Ahora voy a haer un DELETE
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def user(id: str):
-    found = db_client.local.users.find_one_and_delete({"_id": ObjectId(id)})
+    found = users_collection.find_one_and_delete({"_id": ObjectId(id)})
+    #found = db_client.users.find_one_and_delete({"_id": ObjectId(id)})
     
     if not found:
         return {"error": "No se elimina el usuario"}
@@ -101,12 +119,13 @@ async def user(id: str):
 def search_user(field:str, key):
     
     try:
-       user = db_client.local.users.find_one({field: key})
+       user = users_collection.find_one({field: key})
+       #user = db_client.users.find_one({field: key})
        return User(**user_schema(user))
     except: 
         return {"error": "User not found this is empty"}
 
 
  
-if __name__ == "__main__":
-    uvicorn.run(router, host="127.0.0.1", port=8000)
+""" if __name__ == "__main__":
+    uvicorn.run(router, host="127.0.0.1", port=8000) """
